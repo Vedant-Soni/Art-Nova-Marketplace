01b503f80a93c49046c9620c4afc737f6c9efeb2
Vedant-Soni <sonivedant66@gmail.com>
2023-06-10 21:54:12 +0530

    Boiler plate

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { Header } from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import logo from '../images/opensea-logo.svg';
+
+export const Header = () => {
+  return <div></div>;
+};
