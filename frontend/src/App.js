bc96e9450caea72f67e02800f677d2ad162428ae
Vivek kadvani <vkkadvani8866@gmail.com>
2023-06-13 00:30:56 +0530

    design : dropdown of profile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
-import { Header } from './components/Header';
+import Home from './components/Home';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
+      <Header />
+      <Home />
+
     </div>
   );
 }
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,8 +3,10 @@ import openseaLogo from '../images/openseaLogo.png';
 //Styling 
 
 
-export const Header = () => {
+const Header = () => {
   const [menuToggle, setMenuToggle] = useState(false);
+  const [profileDropdown, setDropdown] = useState(false);
+  console.log(profileDropdown);
   return (
     <>
       {menuToggle
@@ -96,7 +98,7 @@ export const Header = () => {
           </div>
         </div>
         :
-        <div className='sm:px-8 md:px-10 lg:px-16 px-4 h-full bg-slate-500 py-3.5'>
+        <div className='sm:px-8 md:px-10 lg:px-16 px-4 h-full bg-white py-3.5'>
 
           <div >
             <nav className=' flex lg:grid lg:grid-cols-3 justify-between'>
@@ -117,51 +119,70 @@ export const Header = () => {
                   <img src={openseaLogo} alt="logo" className='h-fit w-fit' />
                 </div>
                 <div className='ml-2 text-white'>
-                  <p className="font-bold">
+                  <p className="font-bold text-black">
                     OpenSea
                   </p>
                 </div>
                 <div className='w-px h-8 mx-6 bg-gray-500'>
                 </div>
                 <div className='lg:flex hidden'>
-                  <p className='mx-2 text-white'>Drops</p>
-                  <p className='mx-2 text-white'>Stats</p>
+                  <p className='mx-2 text-black'>Drops</p>
+                  <p className='mx-2 text-black'>Stats</p>
                 </div>
                 <div>
                 </div>
               </div>
 
               {/* search bar section  */}
-              <div className=' p-2 rounded-xl md:flex lg:flex items-center bg-white bg-opacity-10 bg-blur-xl hidden '>
-                <div className='mt-2 pr-2 text-white'>
+              <div className=' p-2 rounded-xl md:flex lg:flex items-center bg-white bg-opacity-10 bg-blur-xl hidden border-2 border-gray-400'>
+                <div className='mt-2 pr-2 text-gray-600'>
                   <span class="material-symbols-outlined">
                     search
                   </span>
                 </div>
-                <input className='bg-transparent border-0 outline-0 text-white' placeholder='Search items, collections, and accounts' />
+                <input className='bg-transparent border-0 outline-0 text-gray-600' placeholder='Search items, collections, and accounts' />
               </div>
 
               {/* Wallet and cart section  */}
               <div className='flex justify-self-end'>
-                <div className=' bg-opacity-10 rounded-xl flex items-center bg-white bg-blur-xl text-white'>
-                  <div className='lg:flex hidden'>
+                <div className=' bg-opacity-10 rounded-xl flex items-center bg-white bg-blur-xl border-2 border-gray-400'>
+                  <div className='lg:flex hidden text-gray-700'>
                     <span class="mx-2 material-symbols-outlined">
                       wallet
                     </span>
                     <p className='px-2'>Connect Wallet</p>
                   </div>
-                  <div className='border-solid border-white justify-end lg:border lg:border-y-0 lg:border-r-0 h-full hidden md:flex lg:flex items-center border-opacity-20 mx-2 px-4 '>
-                    <span class="material-symbols-outlined" >
-                      account_circle
-                    </span>
+                  <div onMouseOver={() => setDropdown(true)} onMouseLeave={() => setDropdown(false)} className='h-full'>
+
+                    <div className=' text-gray-600 border-solid border-gray-400 justify-end lg:border lg:border-y-0 lg:border-r-0 h-full hidden md:flex lg:flex items-center border-opacity-100 mx-2 px-4 '>
+                      <span class="material-symbols-outlined" >
+                        account_circle
+                      </span>
+
+
+                    </div>
+
+                    <div class="relative">
+                      <div class="cursor-pointer">
+                      </div>
+                      {profileDropdown ?
+                        <div class="absolute right-0 mt-0 w-40 bg-white border border-gray-200 rounded-md shadow-lg ">
+                          <a class="block px-4 py-2 text-gray-800 hover:bg-gray-200" href="#">Option 1</a>
+                          <a class="block px-4 py-2 text-gray-800 hover:bg-gray-200" href="#">Option 2</a>
+                          <a class="block px-4 py-2 text-gray-800 hover:bg-gray-200" href="#">Option 3</a>
+                        </div>
+                        : <></>}
+                    </div>
+
                   </div>
-                  <div className='border-solid border-white justify-end lg:border border-y-0 border-r-0 h-full lg:hidden md:hidden flex items-center border-opacity-20 mx-2 px-4 '>
+
+                  <div className='border-solid text-gray-600 border-gray-400 justify-end lg:border border-y-0 border-r-0 h-full lg:hidden md:hidden flex items-center border-opacity-20 mx-2 px-4 '>
                     <span class="material-symbols-outlined" >
                       search
                     </span>
                   </div>
                 </div>
-                <div className='ml-2 px-4 bg-opacity-10 rounded-xl flex items-center bg-white bg-blur-xl text-white'>
+                <div className='ml-2 px-4 bg-opacity-10  text-gray-600 rounded-xl flex items-center bg-white bg-blur-xl text-white  border-2 border-gray-400'>
                   <span class="material-symbols-outlined">
                     shopping_cart
                   </span>
@@ -170,24 +191,12 @@ export const Header = () => {
 
             </nav>
 
-          </div>
+          </div >
 
-          <div className=' w-full mt-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5'>
-            <div className='rounded-xl'>
-              <img src='https://nftnow.com/wp-content/uploads/2023/02/Ordinal-Punk-78.png' className='rounded-xl  ' />
-            </div>
-            <div className='rounded-xl '>
-              <img src='https://nftnow.com/wp-content/uploads/2023/02/Ordinal-Punk-78.png' className='rounded-xl hidden sm:block' />
-            </div>
-            <div className='rounded-xl '>
-              <img src='https://nftnow.com/wp-content/uploads/2023/02/Ordinal-Punk-78.png' className='rounded-xl hidden md:block ' />
-            </div>
-            <div className='rounded-xl '>
-              <img src='https://nftnow.com/wp-content/uploads/2023/02/Ordinal-Punk-78.png' className='rounded-xl hidden lg:block' />
-            </div>
-          </div>
-        </div>
+
+        </div >
       }
     </>
   )
 };
+export default Header;
